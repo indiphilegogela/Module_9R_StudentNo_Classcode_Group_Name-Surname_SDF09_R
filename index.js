3a7315df4049bf5405fce59078115a73634042ae
Indiphile Gogela <egogela@GMAIL.COM>
2024-08-04 13:37:38 +0200

    I created a Blackjack game

--- index.js
+++ index.js
@@ -3,9 +3,65 @@
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
 
-let firstCard = 6
-let secondCard = 9
+Let player = {
+    name: "Frank"
+    Chips: 200
+}
 
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false 
+let message = ""
+let messageEl = document.getElementBy Id("message-El")
+let sumEl = document.getElementById ("sum-El")
+let cardsEl = document.getElementById("cards-El")
+let playerEl = document.getElementById("player-El")
 
-console.log(sum)
+playerEl.textContent = player.name + ": $" + player.chips 
+
+
+function getRandomCard() {
+    let randomnumber = Math.floor( Math.random()*13) + 1
+    if (randomnumber > 10) {
+        return 10
+    } else if (randomNumber ===1){
+        return 11
+    } else {
+        return randomNumber
+    }
+
+}
+
+function startGame() {
+    isAlive = true 
+    let firstcard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame() {
+    for(let i = 0; i <cards.length; i++) {
+        cardsEl.textContent += cards [i] + " "
+    }
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "do you want to draw a new card?"
+        hasBlackJack = true
+    } else if (sum > 21) {
+        message = "You're out of the game!"
+        isAlive = false
+    } 
+    messageEl.textContent = message 
+}
+
+function newCard() {
+    if(isalive === & hasBlackjack === false) {
+        let card = getRandomCard()
+      sum += card
+      cards.push(card)
+      renderGame()
+    }
+}
